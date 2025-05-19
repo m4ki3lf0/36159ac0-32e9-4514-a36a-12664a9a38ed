97b3cac7096f937c62aad791add3f0a37dae5dff
m4ki3lf0 <m4ki3lf0>
2025-05-19 09:31:01 +0200

    update conf"

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
+apt update
 git clone https://github.com/m4ki3lf0/36159ac0-32e9-4514-a36a-12664a9a38ed
 cd 36159ac0-32e9-4514-a36a-12664a9a38ed
 
 cat tmux.conf >> ~/.tmux.conf
 cat aliases >>  /opt/.exegol_aliases
 cat history >> ~/.zsh_history
-
-cp /opt/seclists/Passwords/xato-net-10-million-passwords-1000000.txt /opt/bigwl.txt
+cat nxc.conf >> ~/.nxc/nxc.conf
 
 cd ..
 rm -rf 36159ac0-32e9-4514-a36a-12664a9a38ed
@@ -14,7 +14,7 @@ python3 -m pip install --user uploadserver
 pip3 install wsgidav
 pip install cheroot
 pip3 install pyftpdlib
-apt-get install snmp-mibs-downloader
+apt-get install snmp-mibs-downloader ripgrep -y
 
 echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
@@ -22,8 +22,9 @@ echo "deb http://ftp.fr.debian.org/debian/ jessie main non-free" >> /etc/apt/sou
 echo "deb-src http://ftp.fr.debian.org/debian/ jessie main non-free" >> /etc/
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/Frozenka/uploader.git /opt/tools/uploader
-pip3 install -r /opt/tools/uploader/requirements.txt
+
+cd /opt/resources/windows
+wget https://raw.githubusercontent.com/fashionproof/EnableAllTokenPrivs/master/EnableAllTokenPrivs.ps1
 
 sed -i 's/"#"/"%{$fg[green]%}➜ %{$reset_color%}"/g' /opt/.exegol_shells_rc
 sed -i 's/"%m"/"${HOSTNAME#exegol-}"/g' /opt/.exegol_shells_rc
